aabe58a2b6d8ef6d5909383a74ef324e4e7c3f3f
George Higbie <gehigbie@gmail.com>
2017-08-20 20:06:04 +0000

    Add basic gulp tasks for testing

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+
+//Styles
+gulp.task('styles', () => {
+    console.log(`Starting styles task`);
+});
+
+//Scripts
+gulp.task('scripts', () => {
+    console.log(`Starting scripts task`);
+});
+
+
+//Images
+gulp.task('images', () => {
+    console.log(`Startng images task`);
+});
