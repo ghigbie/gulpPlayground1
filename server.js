617ad3258728ca1b240d8c08b1bfa381332db0b8
George Higbie <gehigbie@gmail.com>
2017-08-20 20:39:35 +0000

    Add function for server

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 //this is a "static-server file"
 
 const StaticServer = require('static-server');
+
+const server = new StaticServer({
+    rootPath: './public',
+    port: 3000
+});
+
+server.start(() => {
+    console.log(`The server is listening on port ${server.portT}`);
+});
