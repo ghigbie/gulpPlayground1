ad95e0cbb93e0681f3db90b6317625ad399e35cd
George Higbie <gehigbie@gmail.com>
2017-08-20 21:16:23 +0000

    Create livereload functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const uglify = require('gulp-uglify');
+const livereload = require('gulp-livereload');
 
 //File paths
 let SCRIPTS_PATH = 'public/scripts/**/*.js';
@@ -15,7 +16,8 @@ gulp.task('scripts', () => {
     
     return gulp.src(SCRIPTS_PATH) //This can be either SCRIPTS_PATH or 'public/scripts/*.js'
         .pipe(uglify())  //this pipe calls uglify
-        .pipe(gulp.dest('public/dist')); //this saves contents root of dist
+        .pipe(gulp.dest('public/dist')) //this saves contents root of dist
+        .pipe(livereload()); //this triggers the reload as soon as something gits compressed
 });
 
 
@@ -29,9 +31,10 @@ gulp.task('default', () => {
    console.log(`Starting default task`); 
 });
 
-//Watch
-gulp.task('watch', () => {
+//Watch  --- gulp watch is built into gulp
+gulp.task('watch', () => { 
    console.log('Starting gulp watch...watching everyting!'); 
    require('./server.js');
+   livereload.listen();
    gulp.watch(SCRIPTS_PATH, ['scripts']); //gulp watch needs an array of tasks that need to be run
 });
