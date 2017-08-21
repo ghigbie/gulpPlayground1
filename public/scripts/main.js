86e37825da41462c1ddb2e22ca2a2b6ef848acc7
George Higbie <gehigbie@gmail.com>
2017-08-21 14:43:00 +0000

    Add more js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,7 +11,37 @@ class Person{
     }
 }
 
+class Animal{
+    constructor(animalType){
+        this.animalType = animalType;
+    }
+    makeNoise(){
+        if(typeof this.animalType === 'string'){
+            return `<h1>Yo, this is a ${this.animalType}!!!</h1>`;
+        }else{
+            return `No animal here`;
+        }
+    }
+}
+
+class Plant{
+    constructor(plantType){
+        this.plantType = plantType;
+    }
+    be(){
+        if(typeof this.plantType === 'string'){
+            return `<h2>Yo, this is a ${this.plantType}</h2>`;
+        }else{
+            return `No plant here`;
+        }
+    }
+}
+
 let person = new Person('Morpheus');
+let animal1 = new Animal('Lion');
+let plant1 = new Plant('Cactus');
 
 
 document.write(person.hello());
+document.write(animal1.makeNoise());
+document.write(plant1.be());
